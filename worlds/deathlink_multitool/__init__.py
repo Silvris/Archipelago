7b82bfab7b67d63e12571b9c527a8fe7e41f72e1
Silvris <58583688+Silvris@users.noreply.github.com>
2024-05-03 15:03:19 -0500

    first files, working on other stuff

---
+++ worlds/deathlink_multitool/__init__.py
@@ -0,0 +1,7 @@
+from typing import Dict
+
+from worlds.AutoWorld import World
+from worlds.LauncherComponents import components
+from .client import launch
+
+components
---
+++ worlds/deathlink_multitool/client.py
@@ -0,0 +1,17 @@
+from kivy.app import App
+from kivy.metrics import dp
+
+import Utils
+import typing
+from kvui import ContainerLayout, MainLayout, KivyJSONtoTextParser, ScrollBox
+from kivy.uix.button import Button
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.scrollview import ScrollView
+from kivy.effects.scroll import ScrollEffect
+from kivy.uix.boxlayout import BoxLayout
+
+class DeathLinkMultiTool():
+    
+
+def launch():
+    pass
