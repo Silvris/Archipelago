d8deb1608644a47f00d6d1af87ec0213819b950d
Silvris <58583688+Silvris@users.noreply.github.com>
2025-05-02 18:16:08 -0500

    change cook/paint to useful

--- worlds/kss/items.py
+++ worlds/kss/items.py
@@ -57,8 +57,8 @@ copy_abilities: Dict[str, ItemData] = {
     item_names.parasol: ItemData(BASE_ID + 0x113, ItemClassification.progression),
     item_names.mike: ItemData(BASE_ID + 0x114, ItemClassification.progression),
     item_names.sleep: ItemData(BASE_ID + 0x115, ItemClassification.trap),
-    item_names.paint: ItemData(BASE_ID + 0x116, ItemClassification.progression),
-    item_names.cook: ItemData(BASE_ID + 0x117, ItemClassification.progression),
+    item_names.paint: ItemData(BASE_ID + 0x116, ItemClassification.useful),
+    item_names.cook: ItemData(BASE_ID + 0x117, ItemClassification.useful),
     item_names.crash: ItemData(BASE_ID + 0x118, ItemClassification.progression),
 }
 
