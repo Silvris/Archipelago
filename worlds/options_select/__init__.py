b605e37e3e5e87b15a03369e30b89f10a32bdf8b
Silvris <58583688+Silvris@users.noreply.github.com>
2025-08-11 00:29:17 -0500

    revert core conversion

---
+++ worlds/options_select/__init__.py
@@ -0,0 +1,10 @@
+from worlds.LauncherComponents import components, Component, launch_subprocess, Type
+
+
+def launch_client():
+    from .client import launch
+    launch_subprocess(launch, "Options Creator")
+
+
+components.append(Component("Options Creator", "OptionsCreator", func=launch_client, component_type=Type.TOOL,
+                            description="Visual creator for Archipelago option files."), )
