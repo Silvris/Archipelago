fbd0036ec6564ae1b57490d0fd741e52d49389fc
Silvris <58583688+Silvris@users.noreply.github.com>
2025-08-30 21:30:27 -0500

    move goal setting to set_rules

--- worlds/kdl3/__init__.py
+++ worlds/kdl3/__init__.py
@@ -261,10 +261,6 @@ class KDL3World(World):
 
     def generate_basic(self) -> None:
         self.stage_shuffle_enabled = self.options.stage_shuffle > 0
-        goal = self.options.goal.value
-        goal_location = self.multiworld.get_location(location_name.goals[goal], self.player)
-        goal_location.place_locked_item(KDL3Item("Love-Love Rod", ItemClassification.progression, None, self.player))
-        self.multiworld.completion_condition[self.player] = lambda state: state.has("Love-Love Rod", self.player)
         # this can technically be done at any point before generate_output
         if self.options.allow_bb:
             if self.options.allow_bb == self.options.allow_bb.option_enforced:
