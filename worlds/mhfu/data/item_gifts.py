4b4ae4e1fe72acef61a3cdb331e811c4e5587291
Silvris <58583688+Silvris@users.noreply.github.com>
2025-10-23 01:29:08 -0500

    basic set of deco gifts

--- worlds/mhfu/data/item_gifts.py
+++ worlds/mhfu/data/item_gifts.py
@@ -42,5 +42,44 @@ decoration_gifts = {
     "blademaster decorations": {
         968: 5,  # Grinder Jewel
         964: 5,  # Razor Jewel
-    }
+        966: 5,  # Fencer Jewel
+    },
+    "gunner decorations": {
+        975: 5,  # StrongShot Jewel
+        976: 5,  # Pierce Jewel
+        977: 5,  # Pellet Jewel
+    },
+    "attack decorations": {
+        989: 4,  # Attack Jewel
+    },
+    "high-end attack decorations": {
+        1069: 4,  # Fierce Jewel
+    },
+    "defense decorations": {
+        990: 4,  # Defense Jewel
+    },
+    "high-end defense decorations": {
+        1070: 4,  # Turtle Jewel
+    },
+    "critical decorations": {
+        967: 4,  # Expert Jewel
+    },
+    "high-end critical decorations": {
+        1096: 4,  # Hermit Jewel
+    },
+    "evasion decorations": {
+        1080: 1,  # Dodge Jewel
+        1025: 3,  # Evade Jewel
+        1041: 3,  # Jumping Jewel
+    },
+    "earplug decorations": {
+        993: 5,  # Earplug Jewel
+    },
+    "specialty decorations": {
+        1039: 5,  # Fife Jewel (HH)
+        969: 4,  # Stone Wall Jewel (Lance/GL)
+        970: 5,  # Hard Wall Jewel (Lance/GL)
+        1042: 5,  # Sword Draw Jewel
+        1051: 5,  # Combo Coat Jewel (Bow)
+    },
 }
