e409879c9142d503851fbd00aca19a962208d8d8
Silvris <58583688+Silvris@users.noreply.github.com>
2026-01-30 02:25:02 -0600

    fix half hint handling

--- MultiServer.py
+++ MultiServer.py
@@ -1863,7 +1863,7 @@ def get_client_points(ctx: Context, client: Client) -> int:
     checked_locations = sum(len(ctx.location_checks[client.team, slot]) for slot in ctx.slot_info)
     total_locations = sum(len(ctx.locations[slot]) for slot in ctx.slot_info)
     return (ctx.location_check_points * len(ctx.location_checks[client.team, client.slot]) +
-            (math.floor(ctx.additional_hints[client.team, client.slot]) * ctx.get_hint_cost(client.slot)) +
+            math.floor(ctx.additional_hints[client.team, client.slot] * ctx.get_hint_cost(client.slot)) +
             (math.floor(((checked_locations / total_locations) * 100) * ctx.bonus_hint_percent)) -
             ctx.get_hint_cost(client.slot) * ctx.hints_used[client.team, client.slot])
 
@@ -1872,7 +1872,7 @@ def get_slot_points(ctx: Context, team: int, slot: int) -> int:
     checked_locations = sum(len(ctx.location_checks[team, target_slot]) for target_slot in ctx.slot_info)
     total_locations = sum(len(ctx.locations[target_slot]) for target_slot in ctx.slot_info)
     return (ctx.location_check_points * len(ctx.location_checks[team, slot]) +
-            (math.floor(ctx.additional_hints[team, slot]) * ctx.get_hint_cost(slot)) +
+            math.floor(ctx.additional_hints[team, slot] * ctx.get_hint_cost(slot)) +
             (math.floor(((checked_locations // total_locations) * 100) * ctx.bonus_hint_percent)) -
             ctx.get_hint_cost(slot) * ctx.hints_used[team, slot])
 
