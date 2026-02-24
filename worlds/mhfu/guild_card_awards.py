30fc7ff7c4feb9dce5d15f4a2b2052399e37f2fc
Silvris <58583688+Silvris@users.noreply.github.com>
2026-02-23 21:53:33 -0600

    pep8

--- worlds/mhfu/guild_card_awards.py
+++ worlds/mhfu/guild_card_awards.py
@@ -34,6 +34,7 @@ subspecies = [
     "Terra Shogun Ceanataur",
 ]
 
+
 class GuildCardAward(typing.NamedTuple):
     id: int
     village: tuple[int, int] = (0, 0)
@@ -45,9 +46,12 @@ class GuildCardAward(typing.NamedTuple):
     quests: list[str] | None = None
     grindy: bool = False
 
+
 guild_card_awards: dict[str, GuildCardAward] = {
     "Village Chief's Glove": GuildCardAward(award_start + 1, village=(0, 1)),
-    "Village Chief's Hat": GuildCardAward(award_start + 2, village=(0, 3), quests=["m10206", "m10207", "m10209", "m10304", "m10308", "m10309", "m10318"]),
+    "Village Chief's Hat": GuildCardAward(award_start + 2, village=(0, 3), quests=["m10206", "m10207", "m10209",
+                                                                                   "m10304", "m10308", "m10309",
+                                                                                   "m10318"]),
     "Village Chief's Scarf": GuildCardAward(award_start + 3, village=(0, 5), quests=["m10510"]),
     "Village Chief's Coat": GuildCardAward(award_start + 4, village=(0, 5), quests=["m10510"]),
     "Mane Necklace": GuildCardAward(award_start + 5, monster=["Kirin"]),
@@ -56,8 +60,13 @@ guild_card_awards: dict[str, GuildCardAward] = {
     "Miniature Crown": GuildCardAward(award_start + 8, monster=crowns, grindy=True),
     "Bronze Medal": GuildCardAward(award_start + 9, guild=(0, 1)),
     "Silver Medal": GuildCardAward(award_start + 10, guild=(1, 2), quests=["m01124"]),
-    "Gold Medal": GuildCardAward(award_start + 11, guild=(2, 2), quests=["m02124", "m02228", "m02229", "m02214", "m02215", "m02217", "m02218", "m02219", "m02220", "m02221", "m02222", "m02225", "m02226"]),
-    "Black Belt Badge": GuildCardAward(award_start + 12, training=True, quests=[f"m210{i:02}" for i in [*range(1, 11), *range(16, 24)]]),
+    "Gold Medal": GuildCardAward(award_start + 11, guild=(2, 2), quests=["m02124", "m02228", "m02229", "m02214",
+                                                                         "m02215", "m02217", "m02218", "m02219",
+                                                                         "m02220", "m02221", "m02222", "m02225",
+                                                                         "m02226"]),
+    "Black Belt Badge": GuildCardAward(award_start + 12, training=True, quests=[f"m210{i:02}"
+                                                                                for i in [*range(1, 11),
+                                                                                          *range(16, 24)]]),
     "Expert Badge": GuildCardAward(award_start + 13, quests=[f"m210{i:02}" for i in range(11, 16)], training=True),
     "Legend Badge": GuildCardAward(award_start + 14, quests=[f"m220{i:02}" for i in range(1, 7)], training=True),
     "Rare Species Report": GuildCardAward(award_start + 15, monster=subspecies),
@@ -67,17 +76,21 @@ guild_card_awards: dict[str, GuildCardAward] = {
     "Springnight Carp": GuildCardAward(award_start + 19, village=(0, 5), grindy=True),
     "Dosbiscus": GuildCardAward(award_start + 20),
     "Grateful Letter": GuildCardAward(award_start + 21, guild=(2, 2), grindy=True),
-    "Sage's Bracelet": GuildCardAward(award_start + 22, monster=["Crimson Fatalis"], grindy=True), # ???????????? trusting JP guide here
+    "Sage's Bracelet": GuildCardAward(award_start + 22, monster=["Crimson Fatalis"], grindy=True),
+    # ???????????? trusting JP guide here
     "Wyverian Artisan's Hammer": GuildCardAward(award_start + 23, grindy=True),
-    "Hunter's Progress": GuildCardAward(award_start + 24, area=[11, 20, 21, 6, 17, 3, 4, 18, 5, 19, 2, 16, 13, 15, 14], grindy=True),
+    "Hunter's Progress": GuildCardAward(award_start + 24, area=[11, 20, 21, 6, 17, 3, 4, 18, 5, 19, 2, 16, 13, 15, 14],
+                                        grindy=True),
     "Nekoht's Whiskers": GuildCardAward(award_start + 25, village=(1, 0)),
     "Nekoht's Bell": GuildCardAward(award_start + 26, village=(1, 1), quests=["m11116", "m11117", "m11118"]),
     "Nekoht's Coat": GuildCardAward(award_start + 27, village=(1, 2), quests=["m11226"]),
     "Pokke Village Gourd": GuildCardAward(award_start + 28, village=(1, 2), quests=["m11226"]),
     "Orichalcum Shield": GuildCardAward(award_start + 29, guild=(3, 0)),
     "Platinum Shield": GuildCardAward(award_start + 30, guild=(3, 1), quests=["m03127"]),
-    "Gold Shield": GuildCardAward(award_start + 31, guild=(3, 2), quests=["m03127"]), # little hack here, only locked GR optionals have the Lao as a requirement
-    "Supreme Hunter's Crest": GuildCardAward(award_start + 32, guild=(3, 2), village=(1, 2), grindy=True, quests=["m03127", "m11226"]),
+    "Gold Shield": GuildCardAward(award_start + 31, guild=(3, 2), quests=["m03127"]),
+    # little hack here, only locked GR optionals have the Lao as a requirement
+    "Supreme Hunter's Crest": GuildCardAward(award_start + 32, guild=(3, 2), village=(1, 2), grindy=True,
+                                             quests=["m03127", "m11226"]),
     "Igneous Ring": GuildCardAward(award_start + 33, monster=["Fatalis"]),
     "Ring of Fire": GuildCardAward(award_start + 34, monster=["Crimson Fatalis"]),
     "Powder-White Crown": GuildCardAward(award_start + 35, monster=["White Fatalis"]),
@@ -90,7 +103,8 @@ guild_card_awards: dict[str, GuildCardAward] = {
     "Adventurer's Helm": GuildCardAward(award_start + 42, guild=(3, 2), treasure=True),
     "Trenya's Flag": GuildCardAward(award_start + 43, guild=(3, 2), grindy=True),
     "The Ultimate Catnip": GuildCardAward(award_start + 44, guild=(3, 2), grindy=True),
-    "Letter to my Fearless Leader": GuildCardAward(award_start + 45, guild=(3, 2), grindy=True), # not really but its very time consuming
+    "Letter to my Fearless Leader": GuildCardAward(award_start + 45, guild=(3, 2), grindy=True),
+    # not really but its very time consuming
     "Member's Card": GuildCardAward(award_start + 46, guild=(3, 2)),
     "Wyverian Artisan's Mitten": GuildCardAward(award_start + 47, guild=(3, 2), grindy=True),
     "Hunter's Miracle": GuildCardAward(award_start + 48, area=[26, 27, 28, 29, 30, 31]),
@@ -153,7 +167,9 @@ def create_awards(world: "MHFUWorld") -> None:
             add_rule(loc, lambda state, mons=tuple(award.monster): can_hunt_all_monsters(state, mons, world.player))
         if award.area:
             for area in award.area:
-                quests = [q.qid for q in get_area_quests(sorted(set(world.rank_requirements.keys()) - {(0, 4, 0), (2, 0, 0), (2, 1, 0), (2, 2, 0)}), (area,))]
+                quests = [q.qid for q in get_area_quests(
+                    sorted(set(world.rank_requirements.keys()) - {(0, 4, 0), (2, 0, 0), (2, 1, 0), (2, 2, 0)}),
+                    (area,))]
                 add_rule(loc, lambda state, qids=tuple(quests): can_complete_any_quest(state, qids, world.player))
         if award.quests:
             add_rule(loc, lambda state, qids=tuple(award.quests): can_complete_all_quests(state, qids, world.player))
